26db198559e4256785e3602493d43a34219e7d93
Andrea M <33696524+amc1234@users.noreply.github.com>
2019-08-26 19:06:21 -0400

    Add comments

--- pressure_map_GUI.cs
+++ pressure_map_GUI.cs
@@ -29,32 +29,32 @@ namespace PressureMap
         String str;                                                //To hold the string sent by the Arduino 
         String[] splitter = new String[9];                         //String array to split str 
         char[] separator = new char[] { '@' };                     //Separator sent through the Arduino string to assign values to their corresponding variable 
-        float[] pressure = new float[9];
+        float[] pressure = new float[9];                           //Array to hold the pressure at the sensors
 
         public Form1()
         {
-            InitializeComponent();
-            serialPort1.Open();
-            Thread Loop_Thread = new Thread(Loop);
+            InitializeComponent();                                 //Initialization
+            serialPort1.Open();                                    //Open Serial Port   
+            Thread Loop_Thread = new Thread(Loop);                 //Begin Thread
             Loop_Thread.Start();
         }
 
-        public void Loop()
+        public void Loop()                                         //Infinite loop
         {
             while (true)
             {
-                Display();
-                serialPort1.DataReceived += new SerialDataReceivedEventHandler(Read_String);
+                Display();                                         //Display the pressure map
+                serialPort1.DataReceived += new SerialDataReceivedEventHandler(Read_String);    //Read string sent by Arduino
             }
         }
 
-        public void Display() // obtain array of pressure from backend (?)
+        public void Display()                                      //Display values at each sensor obtained from backend Arduino code
         {
             for(int i = 0; i < pressure.Length; i++)
             {
                 if(pressure[i] >= 200)
                 {
-                    switch (i)
+                    switch (i)                                     //Set pressure map colour. Options are red, orange, yellow where red means there's a lot of pressure at that sensor
                     {
                         case 0:
                             Sensor_1.BackColor = Color.Red;
@@ -116,22 +116,22 @@ namespace PressureMap
             }
         }
 
-        public void Read_String(object sender, EventArgs e)
-        {
-            str = serialPort1.ReadExisting();
-            splitter = str.Split(separator, StringSplitOptions.None);
+        public void Read_String(object sender, EventArgs e)         //Read string sent by Arduino, values are sent as one string each value seperated by an "@"
+        {                                                           //pressure0@pressure1@pressure2...
+            str = serialPort1.ReadExisting();                       //Read from serial port
+            splitter = str.Split(separator, StringSplitOptions.None); //Split the string, seperator = '@' and assign to splitter[]
 
-            float.TryParse(splitter[0], NumberStyles.Any, CultureInfo.InvariantCulture, out pressure[0]);   /*Vin_48V*/
-            float.TryParse(splitter[1], NumberStyles.Any, CultureInfo.InvariantCulture, out pressure[1]);   /*Iin_48V*/
-            float.TryParse(splitter[2], NumberStyles.Any, CultureInfo.InvariantCulture, out pressure[2]);   /*Vin_12V*/
-            float.TryParse(splitter[3], NumberStyles.Any, CultureInfo.InvariantCulture, out pressure[3]);   /*Iin_12V*/
-            float.TryParse(splitter[4], NumberStyles.Any, CultureInfo.InvariantCulture, out pressure[4]);   /*Vin_48V*/
-            float.TryParse(splitter[5], NumberStyles.Any, CultureInfo.InvariantCulture, out pressure[5]);   /*Iin_48V*/
-            float.TryParse(splitter[6], NumberStyles.Any, CultureInfo.InvariantCulture, out pressure[6]);   /*Vin_12V*/
-            float.TryParse(splitter[7], NumberStyles.Any, CultureInfo.InvariantCulture, out pressure[7]);   /*Iin_12V*/
+            float.TryParse(splitter[0], NumberStyles.Any, CultureInfo.InvariantCulture, out pressure[0]);   //Assign pressure[0] = splitter[0]
+            float.TryParse(splitter[1], NumberStyles.Any, CultureInfo.InvariantCulture, out pressure[1]);   //Assign pressure[1] = splitter[1]...
+            float.TryParse(splitter[2], NumberStyles.Any, CultureInfo.InvariantCulture, out pressure[2]);
+            float.TryParse(splitter[3], NumberStyles.Any, CultureInfo.InvariantCulture, out pressure[3]);
+            float.TryParse(splitter[4], NumberStyles.Any, CultureInfo.InvariantCulture, out pressure[4]);
+            float.TryParse(splitter[5], NumberStyles.Any, CultureInfo.InvariantCulture, out pressure[5]);
+            float.TryParse(splitter[6], NumberStyles.Any, CultureInfo.InvariantCulture, out pressure[6]);
+            float.TryParse(splitter[7], NumberStyles.Any, CultureInfo.InvariantCulture, out pressure[7]);
             float.TryParse(splitter[8], NumberStyles.Any, CultureInfo.InvariantCulture, out pressure[8]);
 
-            Thread.Sleep(500);
+            Thread.Sleep(500);                                                                              //Delay so to not spam the serial port. Delay matches Arduino delay.
         }
         private void Form1_Load(object sender, EventArgs e)
         {

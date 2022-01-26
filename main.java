7317df97eea838354e97e00de2a0d69ce08adea5
Natcha Fongsrisin <nfongsrisin@gmail.com>
2022-01-26 15:58:28 -0500

    create hello world in java

--- main.java
+++ main.java
@@ -1,10 +1,2 @@
-package com.company;
-
-public class main {
-
-        public static void main(String[] args)
-        {
-            System.out.println("Hello World!");
-        }
-    }
+package com.company;public class main {
 }
